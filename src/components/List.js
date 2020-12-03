11161489d3c1ad7789af9a3fb3b294748d546697
Hans Kool <Hansjekool@hotmail.com>
2020-12-03 11:42:45 +0100

    Initialize app for grocerie app

---
+++ src/components/List.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function List() {
+  return (
+    <div className="Component">
+      <header className="Component-header">
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <p>Test text2.....</p>
+        {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+      </header>
+    </div>
+  );
+}
+
+export default List;
